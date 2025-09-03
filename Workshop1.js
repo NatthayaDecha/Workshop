ad976af14a2eafdc353b7f913a630d0af3d9591b
Natthaya <66111594@dpu.ac.th>
2025-09-03 14:37:23 +0700

    U

--- Workshop1.js
+++ Workshop1.js
@@ -1,45 +1,4 @@
 //ใช้เฉพาะ ifเท่านั้น
-/*
-let score=99;
-if (score >=100){
-    console.log("GardA")
-}
-if (score >=80){
-    console.log("GardA")
-}
-
-let score1=70;
-if (score1>=79){
-    console.log("GardB")
-}
-if(score1>=70){
-    console.log("GardB")
-}
-
-
-let score2=65;
-if(score2>=60){
-    console.log("GardC")
-}
-if(score2>=69){
-    console.log("GardC")
-}
-
-let score3=55;
-if(score3>=50){
-    console.log("GardD")
-}
-if(score3>=59){
-    console.log("GardD")
-}
-
-let score4=49;
-if(score4<=50);{
-    console.log("F")
-}
-    */
-
-//ใช้เฉพาะ ifเท่านั้น
 
 let score=49;
 if (score >=100){
@@ -68,9 +27,6 @@ if(score>=50){
 if(score>=59){
     console.log("GardD")
 }
-
-
-
 
 if(score<=50);{
     console.log("F")
